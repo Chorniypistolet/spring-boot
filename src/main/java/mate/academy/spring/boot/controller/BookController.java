b5d14ecba14af7261ddae58d80ab2388bde7456f
Chorniypistolet <volkivskiy95@gmail.com>
2024-05-09 21:03:04 +0300

    Corrected HW

--- src/main/java/mate/academy/spring/boot/controller/BookController.java
+++ src/main/java/mate/academy/spring/boot/controller/BookController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/books")
+@RequestMapping("/books")
 public class BookController {
-
     private final BookService bookService;
 
     @GetMapping
--- src/main/java/mate/academy/spring/boot/model/Book.java
+++ src/main/java/mate/academy/spring/boot/model/Book.java
@@ -1,78 +1,25 @@
 package mate.academy.spring.boot.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.RequiredArgsConstructor;
+
 import java.math.BigDecimal;
 
+@RequiredArgsConstructor
 @Entity
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, length = 50)
     private String title;
     private String author;
     private String isbn;
     private BigDecimal price;
+    @Column(length = 100)
     private String description;
     private String coverImage;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 
     @Override
     public String toString() {
