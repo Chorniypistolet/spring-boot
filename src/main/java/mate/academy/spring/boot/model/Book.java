d745bb876b24a9efaec0a5075a04d25d86efbc11
Chorniypistolet <volkivskiy95@gmail.com>
2024-05-30 20:27:27 +0300

    @Getter, @Setter, @ToString

--- src/main/java/mate/academy/spring/boot/model/Book.java
+++ src/main/java/mate/academy/spring/boot/model/Book.java
@@ -7,12 +7,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
-@Data
 @Entity
+@Getter
+@Setter
+@ToString
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted = false")
 @Table(name = "books")
