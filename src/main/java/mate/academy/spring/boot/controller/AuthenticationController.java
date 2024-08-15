cf65f0ccdb2873289df2d6e731d49d8a12c38e72
Chorniypistolet <volkivskiy95@gmail.com>
2024-08-15 19:53:52 +0300

    HW JWT

--- src/main/java/mate/academy/spring/boot/controller/AuthenticationController.java
+++ src/main/java/mate/academy/spring/boot/controller/AuthenticationController.java
@@ -2,14 +2,10 @@ package mate.academy.spring.boot.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import mate.academy.spring.boot.dto.user.UserLoginRequestDto;
-import mate.academy.spring.boot.dto.user.UserLoginResponseDto;
 import mate.academy.spring.boot.dto.user.UserResponseDto;
 import mate.academy.spring.boot.dto.user.UserRegistrationRequestDto;
 import mate.academy.spring.boot.exception.RegistrationException;
-import mate.academy.spring.boot.security.AuthenticationService;
 import mate.academy.spring.boot.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserService userService;
-    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) throws RegistrationException {
         return userService.register(requestDto);
     }
 
-    @PostMapping("/login")
-    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto userLoginRequestDto) {
-        return authenticationService.authenticate(userLoginRequestDto);
-    }
 }
