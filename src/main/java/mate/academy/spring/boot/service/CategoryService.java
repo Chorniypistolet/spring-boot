cb12fade3c38bac6cbdce8fb48a310331a61ff8a
Chorniypistolet <volkivskiy95@gmail.com>
2024-08-28 20:43:42 +0300

    Corrected v4 Spring Boot practice: Add Category model

--- src/main/java/mate/academy/spring/boot/service/CategoryService.java
+++ src/main/java/mate/academy/spring/boot/service/CategoryService.java
@@ -2,10 +2,8 @@ package mate.academy.spring.boot.service;
 
 import mate.academy.spring.boot.dto.category.CategoryDto;
 import mate.academy.spring.boot.dto.category.CategoryRequestDto;
-import org.springframework.stereotype.Service;
 import java.util.List;
 
-@Service
 public interface CategoryService {
     List<CategoryDto> findAll();
 
